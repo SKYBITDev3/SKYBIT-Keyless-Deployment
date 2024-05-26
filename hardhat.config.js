e1dcd2610130966dc417be22fadc175c0ac00995
- <->
2024-05-26 22:39:58 +0700

    Replaced @wagmi/chains with viem/chains

--- hardhat.config.js
+++ hardhat.config.js
@@ -10,8 +10,8 @@ const accounts = { mnemonic: process.env.MNEMONIC || `test test test test test t
 // const accounts = [process.env.PRIVATE_KEY0]
 // const accounts = [process.env.PRIVATE_KEY1, process.env.PRIVATE_KEY2]
 
-// You can add more blockchains to this list if they don't already exist in @wagmi/chains
-const additionalNetworks = { // See https://github.com/wagmi-dev/viem/blob/main/src/chains/index.ts
+// You can add more blockchains to this list if they don't already exist in viem/chains
+const additionalNetworks = { // See https://github.com/wevm/viem/blob/main/src/chains/index.ts
   bttc: {
     chainId: 199,
     url: `https://rpc.bittorrentchain.io`,
@@ -68,8 +68,8 @@ const additionalNetworks = { // See https://github.com/wagmi-dev/viem/blob/main/
   },
 }
 
-const hardhatVerifyBuiltinChains = [`mainnet`, `goerli`, `optimisticEthereum`, `bsc`, `sokol`, `bscTestnet`, `xdai`, `gnosis`, `heco`, `polygon`, `opera`, `hecoTestnet`, `optimisticGoerli`, `moonbeam`, `moonriver`, `moonbaseAlpha`, `ftmTestnet`, `base`, `chiado`, `arbitrumOne`, `avalancheFujiTestnet`, `avalanche`, `polygonMumbai`, `baseGoerli`, `arbitrumTestnet`, `arbitrumGoerli`, `sepolia`, `aurora`, `auroraTestnet`, `harmony`, `harmonyTest`] // https://github.com/NomicFoundation/hardhat/blob/main/packages/hardhat-verify/src/internal/chain-config.ts
-const wagmiToHardhatVerifyTranslation = { // because some names don't match. So we'll use the name in hardhat-verify.
+const hardhatVerifyBuiltinChains = [`mainnet`, `goerli`, `optimisticEthereum`, `bsc`, `sokol`, `bscTestnet`, `xdai`, `gnosis`, `heco`, `polygon`, `opera`, `hecoTestnet`, `optimisticGoerli`, `polygonZkEVM`, `moonbeam`, `moonriver`, `moonbaseAlpha`, `polygonZkEVMTestnet`, `ftmTestnet`, `base`, `chiado`, `holesky`, `arbitrumOne`, `avalancheFujiTestnet`, `avalanche`, `polygonMumbai`, `polygonAmoy`, `baseGoerli`, `baseSepolia`, `arbitrumSepolia`, `sepolia`, `aurora`, `auroraTestnet`, `harmony`, `harmonyTest`] // https://github.com/NomicFoundation/hardhat/blob/main/packages/hardhat-verify/src/internal/chain-config.ts
+const viemChainsToHardhatVerifyTranslation = { // because some names don't match. So we'll use the name in hardhat-verify.
   arbitrum: `arbitrumOne`,
   avalancheFuji: `avalancheFujiTestnet`,
   fantom: `opera`,
@@ -78,17 +78,20 @@ const wagmiToHardhatVerifyTranslation = { // because some names don't match. So 
   harmonyOne: `harmony`,
   optimism: `optimisticEthereum`,
   optimismGoerli: `optimisticGoerli`,
+  polygonZkEvm: `polygonZkEVM`,
+  polygonZkEvmTestnet: `polygonZkEVMTestnet`,
 }
 
 let networks = {}
-const chains = require(`@wagmi/chains`)
+const chains = require(`viem/chains`)
 for (let [chainName, chainData] of Object.entries(chains)) {
   if (![`hardhat`, `localhost`].includes(chainName)) { // "HardhatConfig.networks.hardhat can't have an url"
-    chainName = wagmiToHardhatVerifyTranslation[chainName] === undefined ? chainName : wagmiToHardhatVerifyTranslation[chainName] // change to what hardhat-verify uses
+    chainName = viemChainsToHardhatVerifyTranslation[chainName] === undefined ? chainName : viemChainsToHardhatVerifyTranslation[chainName] // change to what hardhat-verify uses
+    // console.log(chainName.trim())
 
     networks[chainName] = {
       chainId: chainData.id,
-      url: chainData.rpcUrls.public.http[0],
+      url: chainData.rpcUrls.default.http[0],
       accounts,
     }
 
@@ -116,7 +119,7 @@ for (let [chainName, chainData] of Object.entries(chains)) {
 
 networks = { ...networks, ...additionalNetworks }
 
-// RPC URL overrides in case you dont want to use the one in @wagmi/chains:
+// RPC URL overrides in case you dont want to use the one in viem/chains:
 // networks.mainnet.url = `https://eth-mainnet.g.alchemy.com/v2/${process.env.ALCHEMY_API_KEY}`
 
 networks.polygonMumbai.url = `https://polygon-mumbai.blockpi.network/v1/rpc/public`
@@ -150,14 +153,14 @@ module.exports = {
   },
   networks,
   etherscan: {
-    apiKey: { // ADD LINES FOR YOUR DESIRED BLOCKCHAINS HERE
+    apiKey: { // ADD LINES FOR YOUR DESIRED BLOCKCHAINS HERE FOR CONTRACT VERIFICATION ON BLOCKCHAIN EXPLORER. If it's in viemChainsToHardhatVerifyTranslation then write the translated value.
       mainnet: process.env.ETHERSCAN_API_KEY,
       goerli: process.env.ETHERSCAN_API_KEY,
       sepolia: process.env.ETHERSCAN_API_KEY,
       celo: process.env.CELOSCAN_API_KEY,
       celoAlfajores: process.env.CELOSCAN_API_KEY,
-      polygonZkEvm: process.env.ZKEVM_POLYGONSCAN_API_KEY,
-      polygonZkEvmTestnet: process.env.ZKEVM_POLYGONSCAN_API_KEY,
+      polygonZkEVM: process.env.ZKEVM_POLYGONSCAN_API_KEY,
+      polygonZkEVMTestnet: process.env.ZKEVM_POLYGONSCAN_API_KEY,
       bsc: process.env.BSCSCAN_API_KEY,
       bscTestnet: process.env.BSCSCAN_API_KEY,
       avalanche: process.env.SNOWTRACE_API_KEY,
