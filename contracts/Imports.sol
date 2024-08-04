cb71f73da901a5e5a4dc925ab627a5a104fa4f7e
- <->
2024-08-04 18:43:47 +0700

    contracts\Imports.sol: Changed imports from global to explicit

--- contracts/Imports.sol
+++ contracts/Imports.sol
@@ -2,6 +2,6 @@
 pragma solidity ^0.8.24;
 
 // To force hardhat to compile the files into artifacts directory
-import "@axelar-network/axelar-gmp-sdk-solidity/contracts/deploy/Create3Deployer.sol";
-import "@SKYBITDev3/ZeframLou-create3-factory/src/CREATE3Factory.sol";
-import "@openzeppelin/contracts/proxy/ERC1967/ERC1967Proxy.sol";
+import {Create3Deployer} from "@axelar-network/axelar-gmp-sdk-solidity/contracts/deploy/Create3Deployer.sol"; // needed in scripts > deployKeylessly-Create3Factory.js > getCreate3FactoryArtifact
+import {CREATE3Factory} from "@SKYBITDev3/ZeframLou-create3-factory/src/CREATE3Factory.sol"; // needed in scripts > deployKeylessly-Create3Factory.js > getCreate3FactoryArtifact
+import {ERC1967Proxy} from "@openzeppelin/contracts/proxy/ERC1967/ERC1967Proxy.sol"; // needed in upgradeable contract deployment via CREATE3
